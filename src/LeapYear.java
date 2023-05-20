6030782c95eae29c3a51e7e48c5bf88358e679cf
Nshiv <kshiv1105@gmail.com>
2023-05-20 23:41:54 +0530

    leap year is done

---
+++ src/LeapYear.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LeapYear
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Year");
+        int year = sc.nextInt();
+        System.out.println(isLeapYear(year));
+    }
+
+    public static boolean isLeapYear(int year)
+    {
+       if(year<=0)
+       {
+           return  false;
+       } else if (year%4==0 || year%100==0 || year%400==0)
+       {
+          return  true;
+       }
+       else
+       {
+           return false;
+       }
+    }
+}
