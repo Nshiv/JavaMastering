6211fba382909d90951d15bbac4149309aff7e75
Nshiv <kshiv1105@gmail.com>
2023-05-21 01:05:41 +0530

    adding overloading concept for methods

---
+++ src/OverLoadingDemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OverLoadingDemo
+{
+    public static void main(String[] args)
+    {
+        System.out.println(convertToCM(18));
+        System.out.println(convertToCM(5,11));
+    }
+    public static double convertToCM(int inches)
+    {
+      return inches*2.54;
+
+    }
+
+    public static double convertToCM(int foot, int inches )
+    {
+        return  convertToCM((foot*12)+inches);
+
+    }
+
+
+}
