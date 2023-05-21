731d298b26017cb9ee06a62fad504efa50dd2ce0
Nshiv <kshiv1105@gmail.com>
2023-05-21 18:18:15 +0530

    catplaying with no bugs

---
+++ src/CatPlay.java
@@ -0,0 +1,28 @@
+public class CatPlay
+{
+    public static void main(String[] args)
+    {
+        System.out.println(isCatPlaying(false, 26));
+        System.out.println(isCatPlaying(true,10));
+        System.out.println(isCatPlaying(true,25));
+        System.out.println(isCatPlaying(true,-1));
+    }
+
+    public static boolean isCatPlaying(boolean status, int time)
+    {
+        if(status==false || time<0)
+        {
+            return false;
+        } else if (time<25 && time>45)
+        {
+         return false;
+        } else if (time>=25 && time<=45)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
