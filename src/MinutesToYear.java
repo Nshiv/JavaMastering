a81b2d4b50c516b6dfa38cd15aea486530563801
Nshiv <kshiv1105@gmail.com>
2023-05-21 17:41:49 +0530

    updated code

--- src/MinutesToYear.java
+++ src/MinutesToYear.java
@@ -2,12 +2,16 @@ public class MinutesToYear
 {
     public static void main(String[]args)
     {
-
+        System.out.println(getYears(527040));
     }
     public static String getYears(int minutes)
     {
+        int hrs = minutes/60;
+        int days = minutes/1440;
+        int remainingDays = (minutes/1440)%365;
+        int year = minutes/525600;
 
-        return  "";
+        return year +"Y"+remainingDays +"d";
 
     }
 }
