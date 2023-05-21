c1951e77fc119bfdcb475bfc6bf6b0a61e9c12f5
Nshiv <kshiv1105@gmail.com>
2023-05-21 16:54:46 +0530

    adding area calculator

---
+++ src/AreaOverloading.java
@@ -0,0 +1,19 @@
+public class AreaOverloading
+{
+    public static void main(String[] args)
+    {
+        System.out.println(getArea(4,5));
+        System.out.println(getArea(5));
+    }
+
+    public static double getArea(double radious)
+    {
+       double area = (3.14)*(radious*radious);
+       return area;
+    }
+
+    public static double getArea(double x, double y)
+    {
+        return x*y;
+    }
+}
