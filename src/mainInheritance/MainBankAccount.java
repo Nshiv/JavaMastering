d1ecbf278548565c12fe646ad0abecdb82ae5eab
Nshiv <kshiv1105@gmail.com>
2023-05-23 14:33:55 +0530

    added info points

--- src/mainInheritance/MainBankAccount.java
+++ src/mainInheritance/MainBankAccount.java
@@ -7,6 +7,10 @@ public class MainBankAccount
     public static void main(String[] args)
     {
         BankAccount ba = new BankAccount("221122",200, "lorem","test@test.com","83683465");
+        System.out.println(ba.getAccount_number());
+        System.out.println(ba.getCust_name());
+        System.out.println(ba.getEmail());
+        System.out.println(ba.getPhone());
         System.out.println(ba.getAccount_balance());
         ba.depositFunds(2000);
         System.out.println(ba.getAccount_balance());
