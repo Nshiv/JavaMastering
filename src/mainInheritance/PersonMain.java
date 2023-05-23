13d61859c1dd2f512d146917d3157ba809490111
Nshiv <kshiv1105@gmail.com>
2023-05-23 13:56:01 +0530

    Person class

---
+++ src/mainInheritance/PersonMain.java
@@ -0,0 +1,18 @@
+package mainInheritance;
+
+import inheritance.Person;
+
+public class PersonMain
+{
+    public static void main(String[] args)
+    {
+        Person pa = new Person();
+        pa.setFirstName("Lorem");
+        pa.setLastName("Ipsum");
+        pa.setAge(19);
+        System.out.println(pa.getFirstName());
+        System.out.println(pa.getLastName());
+        System.out.println(pa.getfullName());
+        System.out.println(pa.isTeen());
+    }
+}
