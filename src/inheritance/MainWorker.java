0c567b9b64c2accf0e2748b8386b530f1f8f19b8
Nshiv <kshiv1105@gmail.com>
2023-05-26 01:14:47 +0530

    added few comments

--- src/inheritance/MainWorker.java
+++ src/inheritance/MainWorker.java
@@ -9,14 +9,14 @@ public class MainWorker
         System.out.println("age is "+tim.getAge());
         System.out.println("pay is "+ tim.collectPay());
         System.out.println("----------------------------");
-
+//salaried emoployee
         SalariedEmployee data = new SalariedEmployee("npk","11/05/2000","01/01/2023", 5000);
         System.out.println(data);
         System.out.println("age is "+data.getAge());
         System.out.println("paycheck  is "+ data.collectPay());
         data.retire();
         System.out.println("pension pay is "+data.collectPay());
-
+// hourly employee
         HourlyEmployee mary = new HourlyEmployee("Mary","30/01/1994","23/10/2021",15);
         System.out.println(mary);
         System.out.println("age is "+mary.getAge());
