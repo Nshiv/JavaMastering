d7ce3d228b30f55745a825238f9b8d017cb6cbbf
Nshiv <kshiv1105@gmail.com>
2023-05-25 20:36:59 +0530

    Object

---
+++ src/javaObjectClass/Test.java
@@ -0,0 +1,37 @@
+package javaObjectClass;
+
+class Test extends Object {
+    public static void main(String[] jff)
+    {
+      Students stu = new Students("shiv", 33);
+        System.out.println(stu.toString());
+        System.out.println(stu);
+    }
+
+}
+
+class  Students
+{
+    private String name;
+    private int age;
+
+    Students(String name, int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+
+  /*  @Override
+    public String toString() {
+        return super.toString();
+    }*/
+
+    @Override
+    public String toString() {
+        return name + "  and age is "+ age;
+        /*return "Students{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';*/
+    }
+}
