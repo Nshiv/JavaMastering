0b588eeff447c644fd8898cb86264952adce6865
Nshiv <kshiv1105@gmail.com>
2023-05-26 15:48:34 +0530

    added few more cxases

--- src/strings/StringInspectionmethods.java
+++ src/strings/StringInspectionmethods.java
@@ -5,35 +5,38 @@ public class StringInspectionmethods
     public static void main(String [] args)
     {
         getinformation("Current city of mine is Agra");
-        getinformation("");
-        getinformation("    ");
+        //getinformation("");
+        //getinformation("    ");
 
     }
 
     public static void getinformation(String str)
     {
-        //getting lenght of String
+        // 1. getting lenght of String
         int lenght = str.length();
         System.out.printf("Lenght of string is %d%n",lenght);
 
-        //validate string is empty
+        // 2. validate string is empty
         if(str.isEmpty())
         {
             System.out.println("String is empty");
             return;
         }
-        //Validate string is blank
+        // 3.Validate string is blank
         if(str.isBlank())
         {
             System.out.println("string is Blank");
         }
 
-        // finding out charcters asper position
+        // 4. finding out charcters asper position
 
         System.out.printf("Charcter at Position 0 is %c%n",str.charAt(1));
 
-        //Last character
-       System.out.printf("Last Character of string is %c%n",str.charAt(lenght-1));
+        // 5.Last character
+        System.out.printf("Last Character of string is %c%n",str.charAt(lenght-1));
+
+        // Index of A in String
+        System.out.printf("Index of character A is %d%n",str.indexOf("A"));
 
 
 
