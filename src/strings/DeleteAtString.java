af66a13fcaba0257fa4fa71eac9590ce308a7f16
Nshiv <kshiv1105@gmail.com>
2023-05-27 12:55:30 +0530

    few more added

--- src/strings/DeleteAtString.java
+++ src/strings/DeleteAtString.java
@@ -7,8 +7,17 @@ public class DeleteAtString
 
         String str = "Data to test";
         StringBuilder builder = new StringBuilder();
+        //appending dtring to stringbuider class
         builder.append(str);
+        //deleting value of zeros position
         builder.deleteCharAt(0);
+        System.out.println(builder);
+        // inserting deleted element via insert method
+        builder.insert(0,"D");
+        System.out.println(builder);
+
+        // deleting a char sequence via delete menthod with two aruments
+        builder.delete(0,4);
         System.out.println(builder);
     }
 }
