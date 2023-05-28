2a7bc370e71dc24bd286729b47ec3ceaeca92962
Nshiv <kshiv1105@gmail.com>
2023-05-28 23:28:02 +0530

    palindrome added

--- src/stringInterviewProblems/PalindromeString.java
+++ src/stringInterviewProblems/PalindromeString.java
@@ -2,5 +2,20 @@ package stringInterviewProblems;
 
 public class PalindromeString
 {
+ public static void main(String[] args)
+ {
+     System.out.println(getPalindrome("abb"));
+ }
+ public static boolean getPalindrome(String str)
+ {
+     StringBuilder builder = new StringBuilder();
+     builder.append(str);
+     builder.reverse();
+     if(str.equalsIgnoreCase(builder.toString()))
+     {
+        return true;
+     }
+     return false;
 
+ }
 }
