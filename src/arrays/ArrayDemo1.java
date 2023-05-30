bb87ac422ec54ff4aa7c808cf5fb7000f900e5e1
Nshiv <kshiv1105@gmail.com>
2023-05-30 23:57:47 +0530

    few more demo

---
+++ src/arrays/ArrayDemo1.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class ArrayDemo1
+{
+    public static void main(String[] args) {
+        int[] myArray = new int[10];
+        myArray[5]=50;
+
+        double[] myarray1 = new double[10];
+        myarray1[2]=56.89;
+
+
+    }
+}
