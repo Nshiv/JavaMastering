fe2b51a56130756408c637dad8a964c63575dc8c
Nshiv <kshiv1105@gmail.com>
2023-05-30 23:53:25 +0530

    array dec

---
+++ src/arrays/ArrayDemo.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class ArrayDemo
+{
+    public static void main(String[] args) {
+
+        int[] pens;
+        int pilotPens[];
+
+        String[] studentsNames;
+        String teacherNames[];
+
+        int[] studentRollNUmber = new int[10];
+    }
+}
