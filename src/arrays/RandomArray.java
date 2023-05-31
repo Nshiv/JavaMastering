88d0dd4635b8e81c8e0d45ab57a1ce1188905ce4
Nshiv <kshiv1105@gmail.com>
2023-05-31 15:04:42 +0530

    array sorting and creations

--- src/arrays/RandomArray.java
+++ src/arrays/RandomArray.java
@@ -16,6 +16,37 @@ public class RandomArray
         Arrays.sort(array1);
         System.out.println(Arrays.toString(array1));
 
+        // fill method in array
+        int[] secondarray = new int[10];
+        System.out.println(Arrays.toString(secondarray));
+        Arrays.fill(secondarray,2);
+        System.out.println(Arrays.toString(secondarray));
+
+        //creating copy of an array
+        int[] thirdarray = createRandomArray(8);
+        System.out.println(Arrays.toString(thirdarray));
+
+        int[] fourtharray = Arrays.copyOf(thirdarray,thirdarray.length);
+        System.out.println(Arrays.toString(fourtharray));
+
+        //sorting of copied array
+        Arrays.sort(fourtharray);
+        System.out.println(Arrays.toString(thirdarray));
+        System.out.println(Arrays.toString(fourtharray));
+
+        //Partial copy
+        int[] smallArray = Arrays.copyOf(thirdarray,5);
+
+        System.out.println(Arrays.toString(smallArray));
+
+        //Larger array creation
+        int[] lageArray = Arrays.copyOf(thirdarray,20);
+
+        System.out.println(Arrays.toString(lageArray));
+
+
+
+
     }
 
     public static int[]  createRandomArray(int length)
