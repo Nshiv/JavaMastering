a0e35eab1064855a30e7bfb31f90db0dba7370ce
Nshiv <kshiv1105@gmail.com>
2023-06-04 15:43:09 +0530

    Array to array list

---
+++ src/collections/ArrayArrayList.java
@@ -0,0 +1,15 @@
+package collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayArrayList
+{
+    public static void main(String[] args) {
+        String[] students = new String[] {"Rohan", "Sohan","Mohan"};
+        //can use list too but used var keyword
+        var liststu = Arrays.asList(students);
+        System.out.println(liststu);
+
+    }
+}
--- src/collections/OrderOfList.java
+++ src/collections/OrderOfList.java
@@ -3,6 +3,7 @@ package collections;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrderOfList
@@ -14,14 +15,14 @@ public class OrderOfList
         list.addAll(List.of("Functional","Unit","Integration","System INegration"));
         //adding a element
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        //list.add(1);
+       // list.add(2);
+       // list.add(3);
 
         //adding array
 
         String[] newArray = {"data","base","Testing"};
-        list.add(Arrays.toString(newArray));
+       // list.add(Arrays.toString(newArray));
 
         //adding element
         List<Integer> primeNumber = new ArrayList<>();
@@ -31,12 +32,24 @@ public class OrderOfList
         primeNumber.add(5);
         primeNumber.add(7);
         List<Integer> prime = new ArrayList<>();
-        list.addAll(primeNumber);
+        //list.addAll(primeNumber);
 
         System.out.println(list);
 
         System.out.println(list.indexOf(1));
         System.out.println(list.indexOf("Unit"));
-        System.out.println(list.indexOf(newArray));
+        //System.out.println(list.indexOf(newArray));
+
+
+
+        // Natual order crration : sorting
+
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list);
+
+        list.sort(Comparator.reverseOrder());
+        System.out.println(list);
+
+
     }
 }
