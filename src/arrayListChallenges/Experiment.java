cbf60e2c19dcf061da967ad8c23ac8b9c4bc4f6a
Nshiv <kshiv1105@gmail.com>
2023-06-04 21:56:24 +0530

    iterator

---
+++ src/arrayListChallenges/Experiment.java
@@ -0,0 +1,31 @@
+package arrayListChallenges;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Experiment
+{
+    public static void main(String[] args)
+    {
+
+        List list = new ArrayList();
+        list.add("Shiv");
+        list.add("Kumar");
+        list.add(85888);
+        list.add(282006);
+        list.add("Agra-06");
+        list.add("Uttar Pradesh");
+        list.add("India");
+        System.out.println(list);
+
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext())
+        {
+            var element = iterator.next();
+            System.out.println(element);
+        }
+
+
+    }
+}
