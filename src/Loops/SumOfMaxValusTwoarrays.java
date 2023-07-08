565fa37a397c8820313a3d12ee92484a6c918c80
Nshiv <kshiv1105@gmail.com>
2023-07-08 15:08:28 +0530

    a programm is added

---
+++ src/Loops/SumOfMaxValusTwoarrays.java
@@ -0,0 +1,24 @@
+package Loops;
+
+public class SumOfMaxValusTwoarrays
+{
+    public static int getMax(int[] array)
+    {
+        int max = array[0];
+        for(int i=0;i<array.length;i++)
+        {
+            if(array[i]>max)
+            {
+                max=array[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[]args)
+    {
+        int[] array1 =  {45,67,903,56};
+        int[] array2 = {334,564,75,89,23,10};
+        System.out.println(getMax(array1)+getMax(array2));
+    }
+}
